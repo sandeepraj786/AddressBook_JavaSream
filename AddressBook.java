89902c18b1c10a5e5b55e0e6332cc137379e124a
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-27 12:42:17 +0530

    ADDED: Greeting Meassage

---
+++ AddressBook.java
@@ -0,0 +1,12 @@
+package com.company.addressbook;
+
+public class AddressBook extends AddressBookManager {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO ADDRESS BOOK PROBLEM USING JAVA 8 FEATURES LIKE JAVA STREAM");
+    }
+}
+
+
+
+
+
---
+++ AddressBookManager.java
@@ -0,0 +1,5 @@
+package com.company.addressbook;
+
+public class AddressBookManager {
+
+}
