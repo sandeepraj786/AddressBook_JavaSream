df2fffca5cfffd93aa8b1375cd0c455ea9c11f94
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-27 12:48:01 +0530

    ADDED: AddressBookManager class and creatBook method

--- AddressBook.java
+++ AddressBook.java
@@ -1,12 +1,28 @@
 package com.company.addressbook;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class AddressBook extends AddressBookManager {
-    public static void main(String[] args) {
-        System.out.println("WELCOME TO ADDRESS BOOK PROBLEM USING JAVA 8 FEATURES LIKE JAVA STREAM");
+    public static void main(String[] args) throws IOException {
+        AddressBook addressBook = new AddressBook();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("AddressBook By Using Java 8 Stream ");
+        System.out.println("1) Create Book \n" + "2) Add Person \n" + "3) Save Person \n" +
+                "4) Edit Person \n" + "5) Delete Person \n" + "6) Sort ByZip \n" +
+                "7) Sort ByName \n" + "8) Display \n" + "9) Search Person \n" + "10) Show Books \n");
+
+        int ch2 = scanner.nextInt();
+        switch (ch2) {
+            case 1:
+                addressBook.createBook();
+                break;
+            default:
+                System.out.println("Please enter correct choice");
+
+        }
     }
 }
-
-
 
 
 
--- AddressBookManager.java
+++ AddressBookManager.java
@@ -1,5 +1,32 @@
 package com.company.addressbook;
+import java.util.*;
+import java.io.*;
 
 public class AddressBookManager {
+    Scanner scanner = new Scanner(System.in);
+    public String fileName;
+    public File file;
+    public String path = "C:\\csv\\";
+    public BufferedWriter bufferedWriter;
+    public FileWriter fileWriter;
 
+    public String createBook() throws IOException {
+        System.out.print("Enter the desired name of your Book : ");
+        fileName = scanner.next();
+        file = new File(path + fileName + ".csv");
+        file.createNewFile();
+        fileWriter = new FileWriter(file, true);
+        bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write("FirstName");
+        bufferedWriter.write(",Lastname");
+        bufferedWriter.write(",City");
+        bufferedWriter.write(",State");
+        bufferedWriter.write(",Zipcode");
+        bufferedWriter.write(",Phonenumber");
+        bufferedWriter.newLine();
+        System.out.println("Address Book Created ");
+        bufferedWriter.close();
+        fileWriter.close();
+        return fileName;
+    }
 }
